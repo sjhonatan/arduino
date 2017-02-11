f41e4a6b9be3993dd3a2dbd2cdfa0979647f3904
Jhonatan da Silva <jhonatan.s04@aluno.ifsc.edu.br>
2017-02-11 14:35:30 -0200

    carrinho

--- jtk/jtk.ino
+++ jtk/jtk.ino
@@ -1,9 +1,9 @@
 /*
 Jhonatan da Silva
 Last Updated version :
-Thu Feb  9 19:29:43 2017
+Sat Feb 11 14:35:30 2017
 Number of code lines: 
-47
+79
 */
 #include <LiquidCrystal.h>
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
@@ -45,10 +45,42 @@ void loop(){
     }
 }
 float ultrassonic(){
-  digitalWrite(disparar, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(disparar, LOW);
-  tempoTotal = pulseIn(IdaVolta,HIGH);
-  distancia = tempoTotal*0.034/2; // velocidade do som = 340m/s 
-  return distancia;
+
+    digitalWrite(disparar, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(disparar, LOW);
+    tempoTotal = pulseIn(IdaVolta,HIGH);
+    distancia = tempoTotal*0.034/2; // velocidade do som = 340m/s 
+    return distancia;
+}
+
+
+/*
+ Start of the development of the class carrinho 
+
+*/
+class Carrinho{
+    public:
+        carrinho(int EN1,int EN2,int IN1,int IN2,int IN3,int IN4);
+        void velocidade(int v);
+        void f():
+        void t();
+        void e();
+        void d();
+};
+Carrinho::f(){
+    digitalWrite(IN1,HIGH); digitalWrite(IN2,HIGH);
+    digitalWrite(IN3,HIGH); digitalWrite(IN4,LOW);
+}
+Carrinho::t(){
+    digitalWrite(IN1,LOW); digitalWrite(IN2,HIGH);
+    digitalWrite(IN3,LOW); digitalWrite(IN4,HIGH);
+}
+Carrinho::e(){
+    digitalWrite(IN1,LOW); digitalWrite(IN2,HIGH);
+    digitalWrite(IN3,HIGH); digitalWrite(IN4,LOW);
+}
+Carrinho::d(){
+    digitalWrite(IN1,HIGH); digitalWrite(IN2,LOW);
+    digitalWrite(IN3,LOW); digitalWrite(IN4,HIGH);
 }
