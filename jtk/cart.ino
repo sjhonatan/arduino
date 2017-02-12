bde4f4b6f017c3efd7aadd073727ae27b36560fa
Jhonatan da Silva <jhonatan.s04@aluno.ifsc.edu.br>
2017-02-12 20:23:39 -0200

    init class of a cart control

---
+++ jtk/cart.ino
@@ -0,0 +1,44 @@
+/*
+Jhonatan da Silva
+Last Updated version :
+Sun Feb 12 20:23:38 2017
+Number of code lines: 
+37
+*/
+class Carrinho{
+    public:
+        Carrinho();
+        void begin();
+        void f();
+        void b();
+        void l();
+        void r();
+    private:
+        bool _status;
+    };
+
+    Carrinho::Carrinho(){
+
+    }
+    void Carrinho::begin(){
+        Serial.begin(9600);
+    }
+    void Carrinho::f(){
+
+    }
+    void Carrinho::b(){
+
+    }
+    void Carrinho::l(){
+
+    }
+    void Carrinho::r(){
+
+    }
+Carrinho cart();
+void setup(){
+
+}
+void loop(){
+
+}
