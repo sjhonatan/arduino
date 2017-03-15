118efd99f64e411af9307c4f21e0743c0173014f
jhonatan <jhonatan.s04@aluno.ifsc.edu.br>
2017-03-15 13:41:12 -0300

    buffer added

--- ASA/ASA/ASA.ino
+++ ASA/ASA/ASA.ino
@@ -1,21 +1,41 @@
+#!usr/bin/env
+# -*- coding: utf-8 -*-
+/*
+Jhonatan da Silva
+Last Updated version :
+Wed Mar 15 13:41:12 2017
+Number of code lines: 
+32
+*/
 int a0 = 0;
 int a1 = 1;
-int voltage = 0;
-int current = 0;
+int voltage[100];
+int current[100];
+int j = 0;
 int serialRead = 0;
+bool send = false;
 
 void setup(){
   Serial.begin(9600);
 }
 
 void loop(){
-  voltage = analogRead(a0);
-  current = analogRead(a1);
-  if (Serial.available() > 0) {
-    serialRead = Serial.read();
-    if(serialRead == 65){
-       Serial.println(voltage);
-       Serial.println(current);
-    }
+  while(j < 100){
+      voltage[j] analogRead(a0);
+      current[j] analogRead(a1);
+      j++;
   }
+  send = true;
+  j = 0;
+  while(send){
+      if (Serial.available() > 0) {
+        serialRead = Serial.read();
+        if(serialRead == 65){
+            for(j = 0; j < 100; j++){
+                Serial.println(voltage[j]);
+                Serial.println(current[j]);
+            }
+        }
+      }
+   }
 }
